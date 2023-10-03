f2855c6bc472e27b9d6860750e2cb084e9539bc5
SasithDuleepa <duleepajayathissa@gmail.com>
2023-10-03 10:36:41 +0530

    bills

--- routes/billRoutes.js
+++ routes/billRoutes.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const Add = require ('./../controller/bill/addBill');
 const BillAccToId = require ('./../controller/bill/bill_accto_bllId');
+const BillAccToDateRange = require ('./../controller/bill/bill_accto_daterange');
+const BillAccToDateRangeUnpaid = require ('./../controller/bill/bill_daterange_unpaid');
 
 const router = express.Router();
 
 
 router.post('/add', Add);
 router.get('/billaccid', BillAccToId)
+router.get('/billaccdaterange', BillAccToDateRange)
+router.get('/billaccdaterangeunpaid', BillAccToDateRangeUnpaid)
 
 module.exports = router;
